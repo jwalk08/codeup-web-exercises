73a0545e5ac66edde966977f8c666cc8cc62a9fd
Janice Walker <walker.janice@aol.com>
2023-11-14 11:51:52 -0600

    error-loading

--- book-list/js/main.js
+++ book-list/js/main.js
@@ -1,22 +1,30 @@
 import {books} from "../data/books.js";
 
+//creating node adding event listener
 const renderBookElement = book => {
     const bookElement = document.createElement('tr');
     bookElement.innerHTML = `
         <td>${book.title}</td>
-       <td>${book.author}</td>
+       <td>${book.authors}</td>
         <td>${book.categories}</td>
         <td>${book.isbn10}</td>
         <td>${book.published_year}</td>
         <td>
-        <button class="btn btn-sm btn-danger">data-delete>Delete</button>
+        <button class="btn btn-sm btn-danger" data-delete>Delete</button>
          </td>
     `;
     const deleteBtn = bookElement.querySelector("button[data-delete]");
-    deleteBtn.addEventListener('click', e => {
+    deleteBtn.addEventListener("click", (e) => {
         bookElement.remove();
     });
     document.querySelector('#book-list').appendChild(bookElement);
+};
+
+const updateBooks = books => {
+    const categoryInput = document.querySelector('#category');
+    const categoryValue = categoryInput.value;
+    const searchInput = document.querySelector('#search');
+    const searchValue = searchInput.value;
 };
 // MAIN
 (() => {
