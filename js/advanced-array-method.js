c64eefcd34c08e76d97638524bd269acc0baba19
Janice Walker <walker.janice@aol.com>
2023-11-20 10:11:37 -0600

    map-filter

--- js/advanced-array-method.js
+++ js/advanced-array-method.js
@@ -1,53 +1,78 @@
 import {NETFLIX_TITLES} from "../data/netflix-titles";
 
-
-// (() => {
-
-// TWO WAYS TO FILTER AN ARRAY
+(() => {
+    console.log(NETFLIX_TITLES);
+// THE FILTER METHOD
 // const scifiTitles = [];
 // for (let title of NETFLIX_TITLES) {
-//     if (title.listed_in.includes('Sci-Fi')) {
-//         scifiTitles.push(title);
-//     }
+// 	if (title.listed_in.includes("Sci-Fi")) {
+// 		scifiTitles.push(title);
+// 	}
 // }
-// console.log(scifiTitles);
-//
-// const scifiTitles = NETFLIX_TITLES.filter(title => title.listed_in.includes('Sci-Fi'));
-// console.log(scifiTitles);
-//
-// const scifiTitles=NETFLIX_TITLES.filter((title) => {
-//     if (!title.listed_in) {
-//         return false;
-//     }
-//     return title.listed_in.includes("Sci-Fi");
+// const scifiTitles = NETFLIX_TITLES.filter((title) => {
+// 	if (!title.listed_in) {
+// 		return false;
+// 	}
+// 	return title.listed_in.includes("Sci-Fi");
 // });
 // console.log(scifiTitles);
-//
-//
 
-// the find method
-//  const theNextGenerations = NETFLIX_TITLES.find(movie => {
-//      return movie.title.toLowerCase() === 'star trek: the next generation';
-//  });
-// console.log(theNextGenerations);
 
-//    const hasStarTrek = NETFLIX_TITLES.some(movie => {
-//        return movie.title.toLowerCase().includes('star trek') && movie.type.toLowerCase()=== 'movie';
-//    });
+// THE FIND METHOD
+// const theNextGeneration = NETFLIX_TITLES.find((movie) => {
+// 	return movie.title.toLowerCase() === "Star Trek: The Next Generation".toLowerCase();
+// });
+// console.log(theNextGeneration);
+
+// // THE SOME METHOD
+// const hasStarTrek = NETFLIX_TITLES.some((movie) => {
+// 	return movie.title.toLowerCase().includes("star trek") && movie.type.toLowerCase() === "movie";
+// });
 // if (hasStarTrek) {
-//     console.log('There is at least one Star Trek movie in the list');
-// })();
+// 	console.log("It has star trek movies, but star wars is better.");
+// }
 
+// THE MAP METHOD
+    const betterNetflixTitles = NETFLIX_TITLES.map((movie, index) => {
+        const betterObject = {
+            id: index + 1,
+            title: movie.title,
+            type: movie.type,
+            director: movie.director?.split(", "),
+            cast: movie.cast?.split(", "),
+            date_added: movie.date_added,
+            release_year: movie.release_year,
+            rating: movie.rating,
+            duration: movie.duration,
+            categories: movie.listed_in?.split(", "),
+            description: movie.description,
+        };
+        return betterObject;
+    });
+    // console.log(betterNetflixTitles);
 
-//the map method
-const betterNetflixTitles = NETFLIX_TITLES.map((movie) => {
-    const betterObject = {
-        id: movie.show_id,
-        type: movie.type,
-        direct: movie.director,
-        cast: movie.cast.split(","),
-        date_added: movie.date_added,
-
+    // THE REDUCE METHOD
+    // How many action movies did Sylvester Stallone star in?
+    const actionStalloneMovies = betterNetflixTitles.reduce((accumulator, movie) => {
+        if (movie.categories?.includes("Action & Adventure") && movie.cast?.includes("Sylvester Stallone")) {
+            return `${movie.title}, ${accumulator}`;
+        } else {
+            return accumulator;
+        }
+    }, "");
+    console.log(actionStalloneMovies);
+    let longestMovie = {
+        duration: 0
     }
+    for (let title of NETFLIX_TITLES) {
+        if (title.type === "Movie") {
+            duration = parseFloat(title.duration?.split(" ")[0]);
+        }
+        if (duration > parseFloat(longestMovie.duration?.split(" ")[0])) {
+            longestMovie = title;
+        }
+    }
+    console.log(longestMovie);
 
-})
+
+})();
